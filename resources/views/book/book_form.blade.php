254ec6dd87588bf9d3b569fe91b0c4ae504dae2e
elbarbaro <leopoldo.misael.lopez.de.alba@gmail.com>
2020-09-13 18:55:33 -0500

    Styles added to book-form

--- resources/views/book/book_form.blade.php
+++ resources/views/book/book_form.blade.php
@@ -1,67 +1,74 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>New book</title>
-</head>
-<body>
-    <h2>New book</h2>
-    @if(isset($book))
-    <form action="{{action('BookController@update', $book->id)}}" method="post">
-    @else
-    <form action="{{action('BookController@store')}}" method="post">
-    @endif
-        {{ csrf_field() }}
-        @isset($book)
-        {{method_field('PUT')}}
-        @endisset
-        <div>
-            <label for="name">Name</label>
+@extends('layout.master')
+
+@section('data')
+    <div class="col s12">
+        <div class="container">
+            <h4>New book</h4>
             @if(isset($book))
-            <input type="text" name="pname" value="{{$book->name}}">
+            <form action="{{action('BookController@update', $book->id)}}" method="post">
             @else
-            <input type="text" name="pname">
+            <form action="{{action('BookController@store')}}" method="post">
             @endif
+                {{ csrf_field() }}
+                @isset($book)
+                {{method_field('PUT')}}
+                @endisset
+                <div class="input-field">
+                    <label for="name">Name</label>
+                    @if(isset($book))
+                    <input type="text" name="pname" value="{{$book->name}}">
+                    @else
+                    <input type="text" name="pname">
+                    @endif
+                </div>
+                <div class="input-field">
+                    <label for="name">Author</label>
+                    @if(isset($book))
+                    <input id="name" type="text" name="pauthor" value="{{$book->author}}">
+                    @else
+                    <input id="name" type="text" name="pauthor">
+                    @endif
+                </div>
+                <div class="input-field">
+                    <select id="category" name="pcategoryId">
+                    @if(isset($book))
+                        <option selected disabled>Choose a category</option>
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}" {{($category->id == $book->category_id)? 'selected': ''}}>{{$category->name}}</option>
+                        @endforeach
+                    </select>
+                    @else
+                        <option selected disabled>Choose a category</option>
+                        @foreach ($categories as $category)
+                            <option value="{{$category->id}}">{{$category->name}}</option>
+                        @endforeach
+                    </select>
+                    @endif
+                    <label for="category">Category</label>
+                </div>
+                <div>
+                    <label for="publishedDate">Published Date</label>
+                    @if(isset($book))
+                    <input id="publishedDate" type="date" name="ppublishedDate" value="{{$book->published_date}}">
+                    @else
+                    <input id="publishedDate" type="date" name="ppublishedDate">
+                    @endif
+                </div>
+                @if(isset($book))
+                <input class="btn right" type="submit" value="Save">
+                @else
+                <input class="btn right" type="submit" value="Create">
+                @endif
+            </form>
         </div>
-        <div>
-            <label for="name">Author</label>
-            @if(isset($book))
-            <input id="name" type="text" name="pauthor" value="{{$book->author}}">
-            @else
-            <input id="name" type="text" name="pauthor">
-            @endif
-        </div>
-        <div>
-            <label for="category">Category</label>
-            <select id="category" name="pcategoryId">
-            @if(isset($book))
-                <option selected disabled>Choose a category</option>
-                @foreach ($categories as $category)
-                    <option value="{{$category->id}}" {{($category->id == $book->category_id)? 'selected': ''}}>{{$category->name}}</option>
-                @endforeach
-            </select>
-            @else
-                <option selected disabled>Choose a category</option>
-                @foreach ($categories as $category)
-                    <option value="{{$category->id}}">{{$category->name}}</option>
-                @endforeach
-            </select>
-            @endif
-        </div>
-        <div>
-            <label for="publishedDate">Published Date</label>
-            @if(isset($book))
-            <input id="publishedDate" type="date" name="ppublishedDate" value="{{$book->published_date}}">
-            @else
-            <input id="publishedDate" type="date" name="ppublishedDate">
-            @endif
-        </div>
-        @if(isset($book))
-        <input type="submit" value="Save">
-        @else
-        <input type="submit" value="Create">
-        @endif
-    </form>
-</body>
-</html>
+    </div>
+@endsection('data')
+
+@push('scripts')
+    <script type="text/javascript">
+        document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('select');
+            var instances = M.FormSelect.init(elems);
+        });
+    </script>
+@endpush
--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -32,5 +32,6 @@
             @yield('data')
         </div>
         <script src="{{asset('js/materialize.min.js')}}"></script>
+        @stack('scripts')
     </body>
 </html>
